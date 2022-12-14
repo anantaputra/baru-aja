4e4f79ebaab35794d364f9af9ab2a0b6b73dd51c
nan74k <nanta.pslove@gmail.com>
2022-12-14 17:58:48 +0700

    as

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -19,34 +19,34 @@ class GoogleAuthController extends Controller
 
     public function callback()
     {
-        try {
-            $google_user = Socialite::driver('google')->user();
-            $user = User::where('email', $google_user->email)->first();
-            if ($user) {
-                Auth::login($user);
-                return redirect()->intended('/');
+        $google_user = Socialite::driver('google')->user();
+        $user = User::where('email', $google_user->email)->first();
+        if ($user) {
+            Auth::login($user);
+            return redirect()->intended('/');
+        } else {
+            // generate user id
+            $user    = User::latest()->first();
+            if($user){
+                $id_user = explode('-', $user->id_user);
+                $urutan = (int) $id_user[1];
+                $urutan++;
             } else {
-                // generate user id
-                $user    = User::latest()->first();
-                if($user){
-                    $id_user = explode('-', $user->id_user);
-                    $urutan = (int) $id_user[1];
-                    $urutan++;
-                } else {
-                    $urutan = 1;
-                }
-                $id_user = 'USR-' . sprintf("%05s", $urutan);
-
-                $new_user = new User();
-                $new_user->id = $id_user;
-                $new_user->firstname = $google_user->name;
-                $new_user->email = $google_user->email;
-                $new_user->password = Str::random(10);
-                $new_user->save();
-
-                Auth::login($new_user);
-                return redirect()->route('home');
+                $urutan = 1;
             }
+            $id_user = 'USR-' . sprintf("%05s", $urutan);
+
+            $new_user = new User();
+            $new_user->id = $id_user;
+            $new_user->firstname = $google_user->name;
+            $new_user->email = $google_user->email;
+            $new_user->password = Str::random(10);
+            $new_user->save();
+
+            Auth::login($new_user);
+            return redirect()->route('home');
+        }
+        try {
         } catch (Exception $e) {
             // dd($e->getMessage());
             return redirect()->back();
